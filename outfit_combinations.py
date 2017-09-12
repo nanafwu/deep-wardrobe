ba969892ba7d6939e43e9eb70b81609a58d27d8b
Nana Wu <nana@yummly.com>
2017-09-12 13:01:51 -0700

    smaller permutations

--- outfit_combinations.py
+++ outfit_combinations.py
@@ -17,11 +17,11 @@ def get_product_category_to_type():
             'sweatshirts': 'top',
             'jackets': 'coat',
             'womens-outerwear': 'coat',
-            'handbags': 'handbags',
-            'hats': 'hats',
-            'jewelry': 'jewelry',
-            'sunglasses': 'sunglasses',
-            'womens-shoes': 'shoes'}
+            'handbags': 'accessories',
+            'hats': 'accessories',
+            'jewelry': 'accessories',
+            'sunglasses': 'accessories',
+            'womens-shoes': 'accessories'}
 
 
 def is_valid_combo(types):
@@ -30,10 +30,7 @@ def is_valid_combo(types):
                  (types.count('coat') == 2) or \
                  (types.count('shoes') == 2) or \
                  (types.count('dress') == 2) or \
-                 (types.count('hats') == 2) or \
-                 (types.count('jewelry') == 2) or \
-                 (types.count('sunglasses') == 2) or \
-                 (types.count('handbags') == 2) or \
+                 (types.count('accessories') >= 2) or \
                  (types.count('dress') == 1 and
                   types.count('bottom') == 1) or \
                  (types.count('dress') == 1 and
@@ -62,6 +59,7 @@ def get_collection_product_permutations():
     all_products = set([])
     counter = 0
     permutation_categories_all = []
+    categories_counter = []
     for collection_id, collection_products in collection_to_products.items():
         # For every collection, get one item per category
         seen_categories = set([])
@@ -73,48 +71,51 @@ def get_collection_product_permutations():
                 seen_categories.add(category)
 
         # Must have at last 4 unique types of products in collection
-        if len(unique_products) < 4:
+        if len(unique_products) < 3:
             pass
 
-        product_combinations = list(permutations(unique_products, 4))
+        product_combinations = list(permutations(unique_products, 3))
         for combo in product_combinations:
             item1_product_id = combo[0][0]
             item2_product_id = combo[1][0]
             item3_product_id = combo[2][0]
-            item4_product_id = combo[3][0]
+            # item4_product_id = combo[3][0]
 
             item1_type = combo[0][1]
             item2_type = combo[1][1]
             item3_type = combo[2][1]
-            item4_type = combo[3][1]
+            # item4_type = combo[3][1]
 
             item1_image = combo[0][2]
             item2_image = combo[1][2]
             item3_image = combo[2][2]
-            item4_image = combo[3][2]
+            # item4_image = combo[3][2]
 
             combo_types = [category_to_types[cp[1]] for cp in combo]
             if is_valid_combo(combo_types):
-                all_products.add((item1_product_id, item1_image))
-                all_products.add((item2_product_id, item2_image))
-                all_products.add((item3_product_id, item3_image))
-                all_products.add((item4_product_id, item4_image))
+                all_products.add((item1_product_id, item1_image, item1_type))
+                all_products.add((item2_product_id, item2_image, item2_type))
+                all_products.add((item3_product_id, item3_image, item3_type))
+                # all_products.add((item4_product_id, item4_image, item4_type))
 
                 all_collection_permutations.append(
                     (counter, collection_id,
                      item1_product_id, item1_type, item1_image,
                      item2_product_id, item2_type, item2_image,
-                     item3_product_id, item3_type, item3_image,
-                     item4_product_id, item4_type, item4_image
+                     item3_product_id, item3_type, item3_image
+                     # item4_product_id, item4_type, item4_image
                      ))
-                permutation_categories = set([
-                    item1_type, item2_type, item3_type, item4_type])
-                permutation_categories_all.append(permutation_categories)
+                permutation_categories = [
+                    item1_type, item2_type, item3_type
+                    # , item4_type
+                ]
+                permutation_categories_all.append(set(permutation_categories))
+                categories_counter += permutation_categories
                 counter += 1
     random.shuffle(all_collection_permutations)
     permutations_counter = Counter([tuple(per)
                                     for per in permutation_categories_all])
-    return all_collection_permutations, all_products, permutations_counter
+    return all_collection_permutations, all_products, Counter(categories_counter)
 
 
 def write_tsv(rows, file_path):
