225966f1cba82d0a317fa0899b5866b128cfbae3
Nana Wu <nana@yummly.com>
2017-09-14 16:03:06 -0700

    upload images to s3

--- app/stylst/stylst.py
+++ app/stylst/stylst.py
@@ -4,6 +4,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 import cnfg
+import boto3
+
 
 app = Flask(__name__)  # create the application instance :)
 app.config.from_object(__name__)  # load config from this file , flaskr.py
@@ -16,6 +18,70 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+config = cnfg.load(".metis_config")
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=config['s3']['S3_KEY'],
+    aws_secret_access_key=config['s3']['S3_SECRET_ACCESS_KEY']
+)
+
+
+def upload_file_to_s3(file, bucket_name, acl="public-read"):
+    """
+    Docs: http://boto3.readthedocs.io/en/latest/guide/s3.html
+    """
+
+    try:
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
+
+    return "{}{}".format(config['s3']["S3_LOCATION"], file.filename)
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+
+    if "user_file" not in request.files:
+        return "No user_file key in request.files"
+
+    file = request.files["user_file"]
+
+    """
+        These attributes are also available
+
+        file.filename               # The actual name of the file
+        file.content_type
+        file.content_length
+        file.mimetype
+
+    """
+    if file.filename == "":
+        return "Please select a file"
+
+    if file:
+        # file.filename = secure_filename(file.filename)
+        s3bucket = config['s3']["S3_BUCKET"]
+        output = upload_file_to_s3(file, s3bucket)
+        print('Uploaded image to', str(output))
+        return redirect(url_for('show_wardrobe'))
+
+    else:
+        # TODO: Show ERROR message
+        return redirect(url_for('show_wardrobe'))
+
 
 def init_db():
     """Initializes the database."""
@@ -25,7 +91,6 @@ def init_db():
 
 def connect_db():
     """Connects to the specific database."""
-    config = cnfg.load(".metis_config")
     engine = create_engine('postgresql://{}:{}@{}:5432/{}'.format(
         config['db_user'],
         config['db_pwd'],
@@ -67,7 +132,7 @@ def add_entry():
                [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('/'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -82,7 +147,7 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('show_wardrobe'))
     return render_template('login.html', error=error)
 
 
@@ -90,7 +155,7 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_wardrobe'))
 
 
 def get_db():
